0739a856f89f1293cee451d972c0bc297407acf0
PhilippKosarev <philipp.kosarev@gmail.com>
2025-07-25 18:07:24 +0100

    flashcard: remove exception handling from yn_prompt() and add return type hint

--- libjam/flashcard.py
+++ libjam/flashcard.py
@@ -1,16 +1,12 @@
-# Asks user for input
+# Gets user input.
 class Flashcard:
-  def yn_prompt(self, question: str):
-    yes_choices = ['yes', 'y']
-    no_choices = ['no', 'n']
+  def yn_prompt(self, question: str) -> bool:
+    yes_choices = ('yes', 'y')
+    no_choices =  ('no',  'n')
     while True:
-      try:
-        user_input = input(f'{question} [y/n]: ')
-        answer = None
-        if user_input.lower() in yes_choices:
-          return True
-        elif user_input.lower() in no_choices:
-          return False
-      except KeyboardInterrupt:
-        print()
-        exit()
+      user_input = input(f'{question} [y/n]: ').lower()
+      answer = None
+      if user_input in yes_choices:
+        return True
+      elif user_input in no_choices:
+        return False
