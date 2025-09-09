b6938ea295c5a6b38287e1705cad95cee3ccbcd2
PhilippKosarev <philipp.kosarev@gmail.com>
2025-09-09 17:26:56 +0100

    notebook: use configparser non-strict mode in read_ini() instead of doing bullshit

--- libjam/notebook.py
+++ libjam/notebook.py
@@ -2,7 +2,6 @@
 import tomllib
 import configparser
 import json
-import re
 
 # Internal imports
 from .drawer import Drawer
@@ -35,51 +34,14 @@ class Notebook:
   # Reads INI file and returns its contents in the form of a dict.
   # allow_duplicates is only to be used as a last resort due to the performance
   # impact and inaccuracy in results.
-  def read_ini(self, ini_file: str, allow_duplicates=False) -> dict:
+  def read_ini(self, ini_file: str) -> dict:
     data = drawer.read_file(ini_file)
-    parser = configparser.ConfigParser(inline_comment_prefixes=('#', ';'))
-    try:
-      parser.read_string(data)
-    except (
-      configparser.DuplicateSectionError,
-      configparser.DuplicateOptionError,
-    ):
-      if allow_duplicates is True:
-        ini_string = open(ini_file, 'r').read()
-        ini_string = re.sub(';.*', '', ini_string)  # Removing comments
-        ini_sections = ini_string.replace(' =', '=').replace('= ', '=')
-        ini_sections = ini_sections.replace('\n', ';')
-        ini_sections = ini_sections.replace('[', '\n[')
-        ini_sections = ini_sections.removeprefix('\n')
-        ini_sections = ini_sections.split('\n')
-        ini_dict = {}
-        for section in ini_sections:
-          section_name = re.sub('];.*', '', section).replace('[', '')
-          section_name = section_name.upper()
-          ini_dict[section_name] = {}
-          declarations = section.removeprefix(f'[{section_name}];')
-          declarations = declarations.split(';')
-          for declaration in declarations:
-            if declaration == '':
-              continue
-            info = declaration.split('=')
-            name = info[0].lower()
-            value = info[1]
-            ini_dict[section_name][name] = value
-        return ini_dict
-      else:
-        return None
-    except configparser.ParsingError:
-      return None
-    sections = parser.sections()
-    data = {}
-    for section in sections:
-      keys = {}
-      for key in parser[section]:
-        value = parser[section][key]
-        keys[key] = value
-      data[section] = keys
-    return data
+    parser = configparser.ConfigParser(
+      inline_comment_prefixes=('#', ';'),
+      strict=False,
+    )
+    parser.read_string(data)
+    return dict(parser)
 
   # Writes an INI file from a given dict to a given path.
   def write_ini(self, ini_file: str, contents: dict):
