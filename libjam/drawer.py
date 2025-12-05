c596dab8e73049616d8747c3bc70efbe879167a4
PhilippKosarev <philipp.kosarev@gmail.com>
2025-12-05 17:58:08 +0000

    drawer: on-demand import of extract functions for better initial import times and use actual list of supported archives in is_archive_supported()

--- libjam/drawer.py
+++ libjam/drawer.py
@@ -1,18 +1,14 @@
 # Imports
-import io
 import os
 import sys
 import math
 import shutil
 import filetype
 import tempfile
-import zipfile
-import rarfile
-import tarfile
-import py7zr
 import platform
 import platformdirs
 import subprocess
+
 
 # Shorthand vars
 PLATFORM = platform.system()
@@ -41,167 +37,25 @@ def outpath(path: str or list) -> str or list:
     return result_list
 
 
-# Internal classes
-class JamTarFile:
-  def __init__(self, tar: tarfile.TarFile):
-    self.tar = tar
-    self.members = self.tar.getmembers()
-    self.n_members = len(self.members)
-    self.extracted = 0
-    self.archive_basename = None
-
-  @classmethod
-  def open(cls, *args, **kwargs):
-    tar = tarfile.open(*args, **kwargs)
-    self = cls(tar)
-    return self
-
-  def extractall(self, path: str, progress_function: callable = None):
-    self.extract_location = path
-    self.progress_function = progress_function
-    self.tar.extractall(path, filter=self.filter)
-
-  def filter(
-    self, member: tarfile.TarInfo, path: str, /
-  ) -> tarfile.TarInfo | None:
-    if self.progress_function:
-      self.progress_function(self.extracted, self.n_members)
-    self.extracted += 1
-    member = tarfile.data_filter(member, path)
-    if member:
-      if not self.archive_basename:
-        self.archive_basename = os.path.basename(member.name)
-      member.name = member.name.removeprefix(self.archive_basename)
-      member.name = self.extract_location + member.name
-    return member
-
-
-class SevenZipCallbacks(py7zr.callbacks.ExtractCallback):
-  def __init__(self, to_extract: int, progress_function: callable = None):
-    self.extracted = 0
-    self.to_extract = to_extract
-    self.progress_function = progress_function
-
-  def report_start_preparation(self):
-    pass
-
-  def report_start(self, file, size):
-    if self.progress_function is not None:
-      self.progress_function(self.extracted, self.to_extract)
-    self.extracted += 1
-
-  def report_end(self, file, size):
-    pass
-
-  def report_postprocess(self):
-    pass
-
-  def report_update(self, size):
-    pass
-
-  def report_warning(self, message):
-    pass
-
-
-# Extract functions
-def generic_extract(
-  archive_object: zipfile.ZipFile or rarfile.RarFile,
-  extract_location: str,
-  progress_function: callable = None,
-):
-  archived_files = archive_object.namelist()
-  to_extract = len(archived_files)
-  extracted = 0
-  for archived_file in archived_files:
-    if progress_function:
-      progress_function(extracted, to_extract)
-    extracted += 1
-    archive_object.extract(archived_file, path=realpath(extract_location))
-
-
-def extract_zip(
-  archive: str or bytes,
-  extract_location: str,
-  progress_function: callable = None,
-):
-  if type(archive) is str:
-    archive = open(realpath(archive), 'rb').read()
-  archive_object = zipfile.ZipFile(io.BytesIO(archive))
-  generic_extract(archive_object, extract_location, progress_function)
-
-
-def extract_rar(
-  archive: str or bytes,
-  extract_location: str,
-  progress_function: callable = None,
-):
-  if type(archive) is str:
-    archive = open(realpath(archive), 'rb').read()
-  archive_object = rarfile.RarFile(io.BytesIO(archive))
-  generic_extract(archive_object, extract_location, progress_function)
-
-
-def generic_tar_extract(
-  archive: str or bytes,
-  archive_type: str,  # 'gz', 'xz' or ''
-  extract_location: str,
-  progress_function: callable = None,
-):
-  if type(archive) is str:
-    archive = open(realpath(archive), 'rb').read()
-  if archive_type:
-    archive_object = JamTarFile.open(
-      mode=f'r:{archive_type}',
-      fileobj=io.BytesIO(archive),
-    )
-  else:
-    archive_object = JamTarFile.open(
-      mode='r',
-      fileobj=io.BytesIO(archive),
-    )
-  archive_object.extractall(realpath(extract_location), progress_function)
-
-
-def extract_tar(
-  archive: str or bytes,
-  extract_location: str,
-  progress_function: callable = None,
-):
-  generic_tar_extract(archive, '', extract_location, progress_function)
-
-
-def extract_tar_gz(
-  archive: str or bytes,
-  extract_location: str,
-  progress_function: callable = None,
-):
-  generic_tar_extract(archive, 'gz', extract_location, progress_function)
-
-
-def extract_tar_xz(
-  archive: str or bytes,
-  extract_location: str,
-  progress_function: callable = None,
-):
-  generic_tar_extract(archive, 'xz', extract_location, progress_function)
-
-
-def extract_7z(
-  archive: str or bytes,
-  extract_location: str,
-  progress_function: callable = None,
-):
-  if type(archive) is str:
-    archive = open(realpath(archive), 'rb').read()
-  archive_object = py7zr.SevenZipFile(io.BytesIO(archive))
-  archived_files = archive_object.namelist()
-  to_extract = len(archived_files)
-  seven_zip_callbacks = SevenZipCallbacks(to_extract, progress_function)
-  archive_object.extract(
-    realpath(extract_location),
-    recursive=True,
-    callback=seven_zip_callbacks,
+def get_extract_functions() -> dict:
+  from .extract_functions import (
+    extract_zip,
+    extract_rar,
+    extract_tar,
+    extract_tar_gz,
+    extract_tar_xz,
+    extract_7z,
   )
+
+  extract_functions = {
+    'zip': extract_zip,
+    'rar': extract_rar,
+    'tar': extract_tar,
+    'gz': extract_tar_gz,
+    'xz': extract_tar_xz,
+    '7z': extract_7z,
+  }
+  return extract_functions
 
 
 # Deals with files.
@@ -668,7 +522,7 @@ class Drawer:
 
   # Given a path to an archive, returns whether archive's extraction is supported.
   def is_archive_supported(self, path: str) -> bool:
-    supported_archive_types = ['zip', 'rar', '7z']
+    supported_archive_types = list(get_extract_functions().keys())
     filetype = self.get_filetype(path)
     return filetype in supported_archive_types
 
@@ -683,21 +537,16 @@ class Drawer:
     extract_location: str,
     progress_function: callable = None,
   ):
-    extract_functions = {
-      'zip': extract_zip,
-      'rar': extract_rar,
-      'tar': extract_tar,
-      'gz': extract_tar_gz,
-      'xz': extract_tar_xz,
-      '7z': extract_7z,
-    }
+    extract_functions = get_extract_functions()
     archive_type = self.get_filetype(archive)
     function = extract_functions.get(archive_type)
     if function is None:
       raise NotImplementedError(
         f"Archive type '{archive_type}' is not supported."
       )
-    function(archive, extract_location, progress_function)
+    archive = realpath(archive)
+    extract_location = realpath(extract_location)
+    return function(archive, extract_location, progress_function)
 
   # Same as xdg-open, but platform-independent.
   def open(self, argument: str, is_path: bool = True) -> int:
