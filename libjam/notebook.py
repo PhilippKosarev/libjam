f7b2bb9fe7e625124960558e2957e108f78dd123
PhilippKosarev <philipp.kosarev@gmail.com>
2026-02-17 18:13:49 +0000

    notebook: remove usage of removed drawer functions

--- libjam/notebook.py
+++ libjam/notebook.py
@@ -1,123 +1,113 @@
 # Imports
-from enum import Enum
-import sys
+from configparser import ConfigParser
+from pathlib import Path
 import os
-import io
+import sys
 import copy
 import toml
-import configparser
 import json
+import platformdirs
 
 # Internal imports
-from .drawer import Drawer
 from .typewriter import Typewriter
 
 # Shorthand vars
-drawer = Drawer()
 typewriter = Typewriter()
-config_folder = drawer.get_config()
 
 
 # Internal functions
-def merge(source: dict, destination: dict) -> dict:
-  for key, value in source.items():
+def merge_dicts(src: dict, dst: dict) -> dict:
+  for key, value in src.items():
     if isinstance(value, dict):
-      node = destination.setdefault(key, {})
-      merge(value, node)
+      node = dst.setdefault(key, {})
+      merge_dicts(value, node)
     else:
-      destination[key] = value
-  return destination
+      dst[key] = value
+  return dst
 
 
-# Enums
-class ConfigExtension(Enum):
-  TOML = 'toml'
-  INI = 'ini'
-  JSON = 'json'
-
-
-# Deals with configs and reading/writing files
+# Provides functions for reading and writing configuration files as well as
+# per-program configuration management via `Notebook.Ledger`.
 class Notebook:
-  # Returns a tuple of (read, write) functions for a given configuration file.
-  def get_read_write_functions(self, file: str) -> tuple:
-    functions_by_extension = {
+  # Returns a pair of (read, write) functions for a given configuration file.
+  def get_read_write_functions(self, file) -> tuple[callable, callable]:
+    functions = {
       'toml': (self.read_toml, self.write_toml),
       'ini': (self.read_ini, self.write_ini),
       'json': (self.read_json, self.write_json),
     }
-    extension = drawer.get_extension(file)
-    function_pair = functions_by_extension.get(extension)
-    if function_pair is None:
-      raise NotImplementedError(
-        f"Files with extension '{extension}' are not supported."
-      )
-    return function_pair
+    extension = os.path.splitext(file)[1].removeprefix('.')
+    pair = functions.get(extension)
+    if not pair:
+      raise NotImplementedError(f"Unsupported format '{extension}'")
+    return pair
 
   # Returns a toml file parsed to a dict.
-  def read_toml(self, file: str) -> dict:
-    data = drawer.read_file(file)
-    return toml.loads(data)
+  def read_toml(self, file) -> dict:
+    with open(file, 'r') as fp:
+      return toml.load(fp)
 
   # Writes a toml file parsed from a dict.
-  def write_toml(self, path: str, contents: dict, overwrite: bool = False):
-    data = toml.dumps(contents)
-    drawer.write_file(path, data, overwrite)
+  def write_toml(self, file, data: dict):
+    with open(file, 'w') as fp:
+      toml.dump(data, fp)
 
-  # Returns an ini file parsed to a dict.
-  def read_ini(self, ini_file: str) -> dict:
-    data = drawer.read_file(ini_file)
-    parser = configparser.ConfigParser(
+  def _get_ini_parser(self) -> ConfigParser:
+    parser = ConfigParser(
       inline_comment_prefixes=('#', ';'),
       strict=False,
     )
+    return parser
+
+  # Returns an ini file parsed to a dict.
+  def read_ini(self, file) -> dict:
+    with open(file, 'r') as fp:
+      data = fp.read()
+    parser = self._get_ini_parser()
     parser.read_string(data)
     return parser._sections
 
   # Writes a ini file parsed from a dict.
-  def write_ini(self, path: str, contents: dict, overwrite: bool = False):
-    parser = configparser.ConfigParser(
-      inline_comment_prefixes=('#', ';'),
-      strict=False,
-    )
-    parser.read_dict(contents)
-    string_io = io.StringIO()
-    parser.write(string_io)
-    drawer.write_file(path, string_io.getvalue(), overwrite)
+  def write_ini(self, file, data: dict):
+    parser = self._get_ini_parser()
+    parser.read_dict(data)
+    with open(file, 'w') as fp:
+      parser.write(fp)
 
   # Returns a json file parsed to a dict.
-  def read_json(self, path: str) -> dict:
-    data = drawer.read_file(path)
-    return json.loads(data, strict=False)
+  def read_json(self, file) -> dict:
+    with open(file, 'r') as fp:
+      return json.load(fp, strict=False)
 
   # Writes a json file parsed from a dict.
-  def write_json(self, path: str, contents: dict, overwrite: bool = False):
-    data = json.dumps(contents, indent=2)
-    drawer.write_file(path, data, overwrite)
+  def write_json(self, file, data: dict):
+    with open(file, 'w') as fp:
+      data = json.dump(data, fp, indent=2)
 
 
-# Making the ConfigExtension enum available to the user
-Notebook.ConfigExtension = ConfigExtension
 notebook = Notebook()
 
 
 # A config object, meant to be created by the Ledger, but can also be used by
 # itself.
 #
-# On initialisation it checks if the 'file' exists, and if not, then
-# it creates it and writes the contents of 'template' to it. Then it checks
-# the config, using the check() method, for any errors with the config file,
-# if it encounters an issue it prints what the issue is and then exits. In
-# other words, if you have initialised the config object, you can be sure
+# On initialisation, if the `file` does not exist it writes the file using the
+# 'template' string. Then it checks the config for any errors with the config
+# file, if it encounters an issue it prints what the issue is and then exits.
+# In other words, if you have initialised the config object, you can be sure
 # that it does not contain any errors.
+#
+# The `defaults` dict is used to fill in any values that are missing in the
+# config file itself.
 class Config:
-  def __init__(self, file: str, defaults: dict, template: str):
+  def __init__(self, file, defaults: dict, template: str):
     # Ensuring file exists
-    if not drawer.exists(file):
-      drawer.write_file(file, template)
+    file = Path(file)
+    if not file.is_file():
+      file.write_text(template)
     # Setting vars to self
-    self.readwrite_functions = notebook.get_read_write_functions(file)
-    self.defaults = defaults
     self.file = file
+    self.defaults = defaults
     # Checking config contents
     self.check()
 
@@ -125,80 +115,86 @@ class Config:
   # the error and exits using the appropriate exit code.
   def check(self):
     try:
-      function = self.readwrite_functions[0]
-      _ = function(self.file)
+      self.read()
     except Exception as e:
       self.on_error(str(e))
 
-  def on_error(self, error: str or list):
+  def on_error(self, error: str or list[str]):
     error_type = type(error)
+    if error_type not in (str, list):
+      raise TypeError(f"Invalid error type '{error_type}'")
     if error_type is str:
       title = f"Configuration error in '{self.file}':"
-      errors = [error]
-    elif error_type is list:
-      if len(error) == 1:
-        self.on_error(error[0])
-      else:
-        title = f"Configuration errors in '{self.file}':"
-      errors = error
-    else:
-      raise TypeError()
+      error = [error]
     title = typewriter.stylise(
       typewriter.Colour.BRIGHT_RED,
       'Configuration error(s):',
     )
     title = typewriter.bolden(title)
-    formatted_errors = typewriter.list_to_columns(
-      [f'- {error}' for error in errors],
-      n_columns=1,
-    )
-    print(
-      f'{title}\n{self.file}:\n{formatted_errors}',
-      file=sys.stderr,
-    )
-    if hasattr(os, 'EX_CONFIG'):
-      sys.exit(os.EX_CONFIG)
-    else:
-      sys.exit(78)
+    errors = [f'- {e}' for e in error]
+    errors = typewriter.list_to_columns(errors, n_columns=1)
+    print(f'{title}\n{self.file}:\n{errors}', file=sys.stderr)
+    exit_code = getattr(os, 'EX_CONFIG', None)
+    if exit_code is None:
+      exit_code = 78
+    sys.exit(exit_code)
 
   # Reads the config and fills any missing values with the 'defaults' provided
   # during initialisation.
   def read(self) -> dict:
-    function = self.readwrite_functions[0]
-    file_values = function(self.file)
-    default_values = copy.deepcopy(self.defaults)
-    actual_values = merge(file_values, default_values)
-    return actual_values
+    read_func, _ = notebook.get_read_write_functions(self.file)
+    data = read_func(self.file)
+    defaults = copy.deepcopy(self.defaults)
+    data = merge_dicts(data, defaults)
+    return data
 
   # Writes the given dict to the config file.
   # Note that using this function will erase all the comments and custom
   # formatting from the file.
-  def write(self, contents: dict):
-    function = self.readwrite_functions[1]
-    return function(self.file, contents, overwrite=True)
+  def write(self, data: dict):
+    _, write_func = notebook.get_read_write_functions(self.file)
+    return write_func(self.file, data)
 
 
 # Ensures that the program's configuration directory exists on initialisation
 # and simplifies the creation of Config objects.
 class Ledger:
-  def __init__(self, program: str):
-    self.program_dir = f'{config_folder}/{program}'
-    # Making sure that required directories exist
-    drawer.make_folder(config_folder)
-    drawer.make_folder(self.program_dir)
+  def __init__(
+    self,
+    program: str,
+    author: str = None,
+    version: str = None,
+    roaming: bool = False,
+  ):
+    configs_dir = platformdirs.user_config_path()
+    configs_dir.mkdir(exist_ok=True)
+    self.config_dir = platformdirs.user_config_path(
+      program,
+      author,
+      version,
+      roaming,
+    )
 
   def init_config(
     self,
     name: str,
     defaults: dict,
     template: str,
-    extension: Notebook.ConfigExtension = Notebook.ConfigExtension.TOML,
+    extension: str = 'toml',
   ) -> Config:
-    file = f'{self.program_dir}/{name}.{extension.value}'
+    """Initialises a Config.
+
+    The `extension` can be 'toml', 'ini' or 'json'
+    """
+    if extension not in ('toml', 'ini', 'json'):
+      raise ValueError(f"Invalid extension '{extension}'")
+    self.config_dir.mkdir(exist_ok=True)
+    file = name + '.' + extension
+    file = self.config_dir / file
     config = Config(file, defaults, template)
     return config
 
 
-# Making the Ledger and Config classes available to the user
+# Making the Ledger and Config classes available via Notebook
 Notebook.Ledger = Ledger
 Ledger.Config = Config
