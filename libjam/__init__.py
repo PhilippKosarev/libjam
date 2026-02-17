76172d0dc9c3c1b0018a72b6c6a7e94624e4cdad
PhilippKosarev <philipp.kosarev@gmail.com>
2026-02-17 19:08:37 +0000

    remove the clipboard submodule

--- libjam/__init__.py
+++ libjam/__init__.py
@@ -2,15 +2,13 @@
 from .captain import Captain
 from .drawer import Drawer
 from .typewriter import Typewriter
-from .clipboard import Clipboard
 from .notebook import Notebook
 from .flashcard import Flashcard
 from .cloud import Cloud
 
-# Instancing
+# Instantiating
 drawer = Drawer()
 typewriter = Typewriter()
-clipboard = Clipboard()
 notebook = Notebook()
 flashcard = Flashcard()
 cloud = Cloud()
